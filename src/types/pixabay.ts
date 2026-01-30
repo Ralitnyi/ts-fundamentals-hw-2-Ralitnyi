2fd968b64f3455031e3441425039940a55aa0ca4
Ralitnyi Ivan <ralitniyivan@gmail.com>
2026-01-30 15:06:39 +0200

    complete task

--- src/types/pixabay.ts
+++ src/types/pixabay.ts
@@ -1,3 +1,16 @@
-export type PixabayImage = {};
+export interface PixabayImage {
+  id: number;
+  likes: number;
+  webformatURL: string;
+  largeImageURL: string;
+  tags: string;
+  downloads: number;
+  views: number;
+  comments: number;
+}
 
-export type PixabayResponse = {};
+export interface PixabayResponse {
+  total: number;
+  totalHits: number;
+  hits: PixabayImage[];
+}
